05ebc127a7081304cbde2cda22afd24c67ce29d6
Kræn Byskov <kb@parkpark.dk>
2021-05-18 11:22:18 +0200

    Abilities update

--- script.js
+++ script.js
@@ -18,21 +18,44 @@ var requestOptions = {
 
   
   valorantFetch.then((data) => {
-    data.map(({developerName, fullPortrait}) => {
+    // console.log(data)
+    data.map(({developerName, fullPortrait, abilities}) => {
         
+      const Container = CreateElement({className:"card"})
+
         const Title = CreateElement({
             content: developerName,
             elmt: "h1",
         })
+        
+        const abilitieContainer = CreateElement({ className:"abilitieCard"})
+
 
         const Portrait = CreateElement({
-            elmt: "img",
-            className:"Portrait",
-            src: fullPortrait,
-    
+          elmt: "img",
+          className:"Portrait",
+          src: fullPortrait,
+          
         })
-        root.appendChild(Portrait)
-        root.appendChild(Title)
+        
+        abilities.map((abilitie) => {
+          // console.log(abilitie)
+          const abilitieName = CreateElement({
+              content: abilitie.displayName,
+              elmt: "p",
+          })
+          abilitieContainer.appendChild(abilitieName)
+
+        })
+
+
+
+        Container.appendChild(Portrait)
+        Container.appendChild(Title)
+        Container.appendChild(abilitieContainer)
+        if(developerName !== "Hunter_NPE") {
+          root.appendChild(Container)
+        }
         
     })
   })
