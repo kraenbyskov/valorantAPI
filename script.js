4651fc9fb06f6963a5a4f6d395b8736ff03e9ada
Kræn Byskov <kb@parkpark.dk>
2021-05-19 10:59:50 +0200

    Lidt Comments

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
+/* Importer de functioner vi skal bruge */
 import CreateElement from './components/CreateElement.js';
 import FetchMyData from './components/FetchMyData.js';
-
+/* Griber fat i vores Root Div */
 const root = document.getElementById('root');
 
 /* ==========================================================================
@@ -8,22 +9,26 @@ const root = document.getElementById('root');
    ========================================================================== */
 
 const FetchAgents = () => {
+  /* Clear alt indhold i Root */
 	root.innerHTML = '';
+  /* Fetcher Alle Agents */
 	const agentsFetch = FetchMyData({ Endpoint: 'agents' });
-
+  
+  /* Bruger den data vi får ud at vores Fetch */
 	agentsFetch.then((data) => {
-		data.data.map(({ developerName, fullPortrait, abilities, uuid }) => {
-			const Card = CreateElement({
-				className: 'card'
+    data.data.map(({ developerName, fullPortrait, abilities, uuid }) => {
+      const Card = CreateElement({
+        className: 'card'
 			});
-
+      
+      /* Click Function til at vise single Agent */
 			Card.addEventListener('click', () => FetchSingleAgent(uuid));
-
+      
 			const Title = CreateElement({
-				content: developerName,
+        content: developerName,
 				elmt: 'h1'
 			});
-
+      
 			const abilitieContainer = CreateElement({ className: 'abilitieCard' });
 
 			const Portrait = CreateElement({
@@ -32,18 +37,21 @@ const FetchAgents = () => {
 				src: fullPortrait
 			});
 
+      /* Mapper igennem alle Abilities */
 			abilities.map((abilitie) => {
-				// console.log(abilitie)
-				const abilitieName = CreateElement({
-					content: abilitie.displayName,
+        const abilitieName = CreateElement({
+          content: abilitie.displayName,
 					elmt: 'p'
 				});
 				abilitieContainer.appendChild(abilitieName);
 			});
-
+      
+      /* Tilføjer de ting vi har oprettet */
 			Card.appendChild(Portrait);
 			Card.appendChild(Title);
 			Card.appendChild(abilitieContainer);
+      
+      /* Kigger om developerName har navnet Hunter_NPE hvis det ikke har appender det til vores Root Div */
 			if (developerName !== 'Hunter_NPE') {
 				root.appendChild(Card);
 			}
@@ -51,13 +59,18 @@ const FetchAgents = () => {
 	});
 };
 
+/* Køre Vores Fetch Function af alle Agender */
 FetchAgents();
 
 const FetchSingleAgent = (Id) => {
+  /* Fetcher single Agent med ID */
 	const FetchAgent = FetchMyData({ Endpoint: `agents/${Id}` });
+  /* Clear vores root */
 	root.innerHTML = '';
-
+  
+  /* Bruger den data vi har fetchet */
 	FetchAgent.then((agent) => {
+    /* destructur vores Agent.data  */
 		const { displayName, fullPortrait } = agent.data;
 
 		const Container = CreateElement({
@@ -79,9 +92,11 @@ const FetchSingleAgent = (Id) => {
 			className: 'Button',
 			content: 'Back Button'
 		});
-
+    
+    /* Tilbage knap der køre vores Function til at fetche alle Agents  */
 		BackButton.addEventListener('click', () => FetchAgents());
-
+    
+    /* Tilføjer de create elementer til vores Root  */
 		Container.appendChild(Title);
 		Container.appendChild(Portrait);
 		Container.appendChild(BackButton);
